824d4e351175ed32e4c02959d63f87b18c552e14
grburgess <jmichaelburgess@gmail.com>
2017-04-18 13:30:52 +0200

    added doc string

--- array_to_cmap.py
+++ array_to_cmap.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def array_to_cmap(values,cmap,use_log=False):
+    """
+    Generates a color map and color list that is normalized
+    to the values in an array. Allows for adding a 3rd dimension
+    onto a plot
+    
+    :param values: a list a values to map into a cmap
+    :param cmap: the mpl colormap to use
+    :param use_log: if the mapping should be done in log space
+
+    """
     
     if use_log:
         
