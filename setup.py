216f3dbc15c8d0c9a6e7980499000a524f1d8b4b
grburgess <jmichaelburgess@gmail.com>
2017-05-21 09:38:09 +0200

    update setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='python_tricks',
+    version='0.1',
+    description='Various python tricks I often use and often forget',
+    author='J. Michael Burgess',
+    author_email='jmichaelburgess@gmail.com',
+    packages=['python_tricks'], )
