b5ce199c528e09cc20ee03b01374a84009fddb56
grburgess <jmichaelburgess@gmail.com>
2017-05-21 09:34:17 +0200

    added sort human

---
+++ python_tricks/__init__.py
@@ -0,0 +1,6 @@
+from sort_human import sort_human
+from array_to_cmap import array_to_cmap
+from journal_plots import set_journal, reset
+
+
+__all__  = ['sort_human', 'array_to_cmap', 'set_journal', 'reset']
---
+++ python_tricks/sort_human.py
@@ -0,0 +1,9 @@
+def sort_human(l):
+    """
+    sort a list with indices and letters the way a human would
+    :param l: a list of string
+    """
+    convert = lambda text: float(text) if text.isdigit() else text
+    alphanum = lambda key: [ convert(c) for c in re.split('([-+]?[0-9]*\.?[0-9]*)', key) ]
+    l.sort(key=alphanum)
+    return l
